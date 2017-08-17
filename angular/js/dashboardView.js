eb1be362b580d481909112c24ed0a8159d893f47
Jon Davis <jonerikdavis@gmail.com>
2017-08-17 16:24:41 -0700

    ui updates

--- angular/js/dashboardView.js
+++ angular/js/dashboardView.js
@@ -3,7 +3,20 @@ var dashboardView = {
 
   init: function (app, $scope) {
     $scope.cameras = app.cameras;
+    $scope.modes = ["live-feed", "arm"];
     $scope.noCamera = function () { return Object.keys(app.cameras).length === 0; }
+    $scope.update = function(value, camera) {
+      if (value == "live-feed") {
+        app.controlPreview(camera.info.id);
+        camera.control.preview.enabled = true;
+        camera.control.arm.enabled = false;
+      }
+      else {
+        app.controlArm(camera.info.id);
+        camera.control.preview.enabled = false;
+        camera.control.arm.enabled = true;
+      }
+    };
     $scope.switchPreview = function (cameraId) {
       app.controlPreview(cameraId);
     };
