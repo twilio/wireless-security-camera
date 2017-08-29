ceab10d34bb50bf844efb2c511a05329d4298569
Miao ZhiCheng <zmiao@twilio.com>
2017-08-29 15:14:45 +0300

    syncrhonized camera mode selection
    - There are two user level camera mode: live-feed and arm
    - There are still 2 separated controls in sync object model just to
      confuse people ;)
    - You could test the synchronization by either using
      - Two browsers sessions simulteneously
      - Or force-refresh the browser to see if the option stays

--- angular/js/dashboardView.js
+++ angular/js/dashboardView.js
@@ -3,20 +3,9 @@ var dashboardView = {
 
   init: function (app, $scope) {
     $scope.cameras = app.cameras;
-    $scope.modes = ["live-feed", "arm"];
     $scope.noCamera = function () { return Object.keys(app.cameras).length === 0; }
-    $scope.update = function(value, camera) {
-      if (value == "live-feed") {
-        camera.control.preview.enabled = true;
-        camera.control.arm.enabled = false;
-      }
-      else {
-        camera.control.preview.enabled = false;
-        camera.control.arm.enabled = true;        
-      }
-      app.controlPreview(camera.info.id);
-      app.controlArm(camera.info.id);
-      console.log(camera.info)
+    $scope.changeCameraMode = function(camera) {
+      app.syncCameraMode(camera.info.id);
     };
     $scope.img_oninit = function (camera) {
         camera.snapshotLoadingInProgress = false;
@@ -24,12 +13,6 @@ var dashboardView = {
     $scope.img_onloaded = function (camera) {
         console.info("Image loaded: " + camera.snapshot.img_url);
         camera.snapshotLoadingInProgress = false;
-    };
-    $scope.switchPreview = function (cameraId) {
-      app.controlPreview(cameraId);
-    };
-    $scope.switchArm = function (cameraId) {
-      app.controlArm(cameraId);
     };
     $scope.disarm = function (cameraId) {
       app.disarm(cameraId);
